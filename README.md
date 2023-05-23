d454220ef281d3af95794b40ca4ad014df1dfdcd
kruemelmann <kruemelmannlp@gmail.com>
2023-05-23 12:52:04 +0200

    initial commit of pdfgoose

---
+++ README.md
@@ -0,0 +1,22 @@
+# pdfgoose
+
+pdfgoose is a free and open-source pdfviewer and pdfeditor.
+
+## Installation
+
+```bash
+echo "TODO"
+```
+
+## Contributing
+
+Pull requests welcome:)
+If you like to do some bigger changes please open an issue, so we can chat about.
+
+## Authors
+
+* kruemelmann - [@kruemelmann](https://github.com/kruemelmann/)
+
+## License
+[Apache-2.0](https://choosealicense.com/licenses/apache-2.0/)
+
---
+++ ui/public/electron.js
@@ -0,0 +1,63 @@
+const { app, BrowserWindow, protocol } = require("electron");
+const path = require("path");
+const url = require("url");
+
+// Create the native browser window.
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  // In production, set the initial browser path to the local bundle generated
+  // by the Create React App build process.
+  // In development, set it to localhost to allow live/hot-reloading.
+  const appURL = app.isPackaged
+    ? url.format({
+        pathname: path.join(__dirname, "index.html"),
+        protocol: "file:",
+        slashes: true,
+      })
+    : "http://localhost:3000";
+  mainWindow.loadURL(appURL);
+
+  // Automatically open Chrome's DevTools in development mode.
+  if (!app.isPackaged) {
+    mainWindow.webContents.openDevTools();
+  }
+}
+
+// Setup a local proxy to adjust the paths of requested files when loading
+// them from the local production bundle (e.g.: local fonts, etc...).
+function setupLocalFilesNormalizerProxy() {
+  protocol.registerHttpProtocol(
+    "file",
+    (request, callback) => {
+      const url = request.url.substr(8);
+      callback({ path: path.normalize(`${__dirname}/${url}`) });
+    },
+    (error) => {
+      if (error) console.error("Failed to register protocol");
+    }
+  );
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  setupLocalFilesNormalizerProxy();
+
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
