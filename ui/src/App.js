3f085a950cf15e44cef0abd1729c9356137d3e83
kruemelmann <kruemelmannlp@gmail.com>
2023-05-23 17:34:21 +0200

    adding basic navbar

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './components/navbar/Navbar.js'
 
 function App() {
   return (
     <div className="App">
-        <h1>Hello World</h1>
+        <Navbar />
     </div>
   );
 }
